760f2797420b6536c389c698e578fdbbb8e2576c
Aswin Karuvally <aswinbabuk@gmail.com>
2020-10-13 22:17:36 +0530

    added all the imports

---
+++ WidgetsFun/fun_widgets.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Fun with some of the widgets Qt5 provides
+
+# Import serious stuff
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+
