f426dc03a7c199f1e8badc96b0c8fad235393539
Aswin Karuvally <aswinbabuk@gmail.com>
2020-10-13 22:24:17 +0530

    added widgets

--- WidgetsFun/fun_widgets.py
+++ WidgetsFun/fun_widgets.py
@@ -7,3 +7,32 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
 
+
+# Our main class
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        # Initialize the main window
+        super(MainWindow, self).__init__(*args, **kwargs)
+        
+        # Set title for our window
+        self.setWindowTitle("Awesome App 2.0")
+
+        # Set the layout and widgets
+        layout = QVBoxLayout()
+        widgets = [
+            QCheckBox,
+            QComboBox,
+            QDateTimeEdit,
+            QDial,
+            QDoubleSpinBox,
+            QFontComboBox,
+            QLCDNumber,
+            QLabel,
+            QLineEdit,
+            QProgressBar,
+            QPushButton,
+            QRadioButton,
+            QSlider,
+            QSpinBox,
+            QTimeEdit
+        ]
